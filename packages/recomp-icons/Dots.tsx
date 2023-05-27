e0bc8668bdc4785efe6e7c5416035647811a580f
Jonathan Lacombe <16738161+LacombeJ@users.noreply.github.com>
2023-05-27 03:38:51 -0400

    Add icons

---
+++ packages/recomp-icons/Dots.tsx
@@ -0,0 +1,21 @@
+import * as React from 'react';
+
+export const Dots = () => {
+  <svg
+    xmlns="http://www.w3.org/2000/svg"
+    className="icon icon-tabler icon-tabler-dots"
+    width="32"
+    height="32"
+    viewBox="0 0 24 24"
+    strokeWidth="1"
+    stroke="currentColor"
+    fill="none"
+    strokeLinecap="round"
+    strokeLinejoin="round"
+  >
+    <path stroke="none" d="M0 0h24v24H0z" fill="none"></path>
+    <path d="M5 12m-1 0a1 1 0 1 0 2 0a1 1 0 1 0 -2 0"></path>
+    <path d="M12 12m-1 0a1 1 0 1 0 2 0a1 1 0 1 0 -2 0"></path>
+    <path d="M19 12m-1 0a1 1 0 1 0 2 0a1 1 0 1 0 -2 0"></path>
+  </svg>;
+};
--- packages/recomp-icons/index.ts
+++ packages/recomp-icons/index.ts
@@ -2,11 +2,13 @@ export { default as Caret } from './Caret';
 export { default as Chevron } from './Chevron';
 export { default as Close } from './Close';
 export { default as Collapse } from './Collapse';
+export { Dots } from './Dots';
 export { default as Expand } from './Expand';
 export { default as File } from './File';
 export { default as Folder } from './Folder';
 export { default as Maximize } from './Maximize';
 export { default as Minimize } from './Minimize';
 export { default as Heart } from './Heart';
+export { X } from './X';
 
 declare module '@recomp/icons';
